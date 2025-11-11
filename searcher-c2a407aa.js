4d5de1dae91d551c835fe950b595643211394ae1
giob22 <giovanni.fragola.nl@gmail.com>
2025-11-11 13:15:11 +0100

    prova

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c2b90f7e.js',
+                path_to_root + 'searchindex-f926f26c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
