b7bfd4b52ac0e09c7ba285540426aacb39062f0e
giob22 <giovanni.fragola.nl@gmail.com>
2025-11-15 12:18:07 +0100

    aggiornamento automatico

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0b2c35a8.js',
+                path_to_root + 'searchindex-e3603597.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
